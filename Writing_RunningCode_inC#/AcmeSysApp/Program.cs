b357fe098bad0462393d5e75489426c9a07ed070
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-01 14:27:18 -0700

    Welcome screen for a new accounting software
    An exercise on writing and running code in C#.

---
+++ Writing_RunningCode_inC#/AcmeSysApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AcmeSysApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to ACME Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
