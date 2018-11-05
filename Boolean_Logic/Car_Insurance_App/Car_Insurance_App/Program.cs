48c368cf97031f79f7106db8bcc473c2b55c870d
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-05 15:33:43 -0800

    Car_Insurance approval program
    An exercise on Boolean Logic.

---
+++ Boolean_Logic/Car_Insurance_App/Car_Insurance_App/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Car_Insurance_App
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Tech Academy Auto Insurance");
+
+            Console.WriteLine("What is your age?");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had DUIs? Please answer \"True\" or \"False\".");
+            bool dui = bool.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int ticket = int.Parse(Console.ReadLine());
+            
+            bool qualified = (age > 15) && (!dui) && (ticket <= 3);
+            Console.WriteLine("Qualified: " + qualified);
+
+            
+            Console.Read();
+        }
+    }
+}
