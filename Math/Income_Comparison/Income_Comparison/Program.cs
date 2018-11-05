7ffe980cb428ac7fbb40206641a5985e0aea02a4
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-05 13:24:23 -0800

    Anonymous Income Comparison Program
    An exercise on math and comparison operators.

---
+++ Math/Income_Comparison/Income_Comparison/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Income_Comparison
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Person1
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate");
+            int hrRate1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Hours worked per week");
+            int hrPerWeek1 = Int32.Parse(Console.ReadLine());
+
+            //Person2
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Rate");
+            int hrRate2 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Hours worked per week");
+            int hrPerWeek2 = Int32.Parse(Console.ReadLine());
+
+            // Annual salaries
+            int annualpay1 = hrRate1 * hrPerWeek1 * 52;
+            Console.WriteLine("Annual salary for Person 1 is " + annualpay1);
+            int annualpay2 = hrRate2 * hrPerWeek2 * 52;
+            Console.WriteLine("Weekly salary for Person 1 is " + annualpay2);
+
+            //The comparison
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            bool compare_Income = annualpay1 > annualpay2;
+            Console.WriteLine(compare_Income.ToString());
+
+            // wait for me to read
+            Console.Read();
+        }
+    }
+}
---
+++ Math/Income_Comparison/Income_Comparison/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Income_Comparison")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Income_Comparison")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("02b621b4-e8a6-452c-abf3-5c0db3ef0b33")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
