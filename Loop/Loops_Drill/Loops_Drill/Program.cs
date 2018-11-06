31ea438da59e2988abac2a3c8f9799c017509558
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-05 21:55:38 -0800

    Loops_Drill
    An exercise on while loop and do while loop.

---
+++ Loop/Loops_Drill/Loops_Drill/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Loops_Drill
+{
+    class Program
+    {
+        static void Main()
+        {
+            string do_it_Again = "";
+            do
+            {
+                Console.WriteLine("Give me a number above 200 and you will get a beautiful pattern of even numbers.");
+                int chosenNumber = int.Parse(Console.ReadLine());
+                int i = 0;
+
+                while (i <= chosenNumber)
+                {
+                    Console.Write(i + ">>>>>>>>>>>>>>>>>>######################>>>>>>>>>>>>>>>>>>");
+                    i += 2;
+                }
+                do
+                {
+                    Console.WriteLine("Do you want to do it again? Please write Yes or No? ");
+                    do_it_Again = Console.ReadLine();
+                    if (do_it_Again != "Yes" && do_it_Again != "No")
+                    {
+                        Console.WriteLine("Invalid choice. Please check the spelling.");
+                    }
+                } while (do_it_Again != "Yes" && do_it_Again != "No");
+            } while (do_it_Again == "Yes");
+        }
+    }
+}
