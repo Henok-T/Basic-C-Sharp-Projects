4e193c54e2a8cebbccef247972aeaa964e3aadf8
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-05 18:31:29 -0800

    Shipping quote app
    An exercise on branching (if /ese statements in C#).

---
+++ Branching/Shipping_Quote/Shipping_Quote/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Shipping_Quote
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:");
+            int packageWeight = int.Parse(Console.ReadLine());
+
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package to heavy to be shipped via Package Express. Have a good day.");
+                System.Threading.Thread.Sleep(5000);
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Please enter the package width:");
+            int packageWidth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height:");
+            int packageHeight = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length:");
+            int packageLength = int.Parse(Console.ReadLine());
+
+            // calcutating package dimension
+            int packageDimension = packageLength + (2 * packageWidth) + (2 * packageHeight);
+
+            if (packageDimension > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                System.Threading.Thread.Sleep(5000);
+                Environment.Exit(0);
+            }
+            else
+            {
+                // Pricing
+                float quotePrice = packageDimension * packageWeight / 100;
+                Console.WriteLine("Your estimated total for shipping this package is: " + "$" + quotePrice);
+                Console.Read();
+            }
+        }
+    }
+}
