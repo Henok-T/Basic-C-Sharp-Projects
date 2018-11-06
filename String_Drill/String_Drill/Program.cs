57ad168ebe6d55c986ea8af88189c82059e62d66
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-06 15:03:43 -0800

    String_Drill
    An exercise on Strings in C#.

---
+++ String_Drill/String_Drill/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace String_Drill
+{
+    class Program
+    {
+        static void Main()
+        {
+            string title = "Coffee Ipsum\n";
+            string sentence1 = "Percolator coffee whipped variety robust eu\n";
+            string sentence2 = "froth sweet, variety spoon a redeye.";
+            Console.WriteLine(title.ToUpper() + sentence1 + sentence2);
+            Console.Read(); // just to make a space between
+
+            StringBuilder sb = new StringBuilder("Lorem Ipsum\n");
+
+            sb.Append("This is sentence 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n");
+            sb.Append("This is sentence 2. Sed ante lectus, volutpat in justo non, vulputate imperdiet lectus.\n");
+            sb.Append("This is sentence 3. Mauris a magna augue. Quisque posuere ultricies pharetra.");
+            Console.WriteLine(sb);
+            System.Threading.Thread.Sleep(10000);
+            Console.Read();
+
+        }
+    }
+}
