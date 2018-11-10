0ea2b351d587d99c951641645fe3a6dc43040b2e
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-10 02:21:47 -0800

    Class, Object , Method Drill
    An exercise on C#.

---
+++ ClassObjMetho_Drill/ClassObjMetho_Drill/MathWorkOut.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjMetho_Drill
+{
+    class MathWorkOut
+    {
+        public int Sum(int num1)
+        {
+            return num1 + 25;
+        }
+
+        public int Product(int num1)
+        {
+            return num1 * 25;
+        }
+
+        public int Quotient(int num1)
+        {
+            return num1 / 25;
+        }
+    }
+
+}
---
+++ ClassObjMetho_Drill/ClassObjMetho_Drill/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjMetho_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("please enter a number:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            MathWorkOut operation = new MathWorkOut();
+            Console.WriteLine("\n\t" + num1 + " +" + " my secret number 25 gives us: " + operation.Sum(num1));
+            Console.WriteLine("\n\t" + num1 + " *" + " my secret number 25 gives us: " + operation.Product(num1));
+            Console.WriteLine("\n\t" + num1 + " /" + " my secret number 25 gives us: " + operation.Quotient(num1));
+
+            Console.ReadLine();
+        }
+    }
+}
