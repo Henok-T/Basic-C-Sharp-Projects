9e968b86554d6dc4ddbd0645f2c5fcb2475840c5
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-12 13:33:28 -0800

    Class/Method Drill2
    An exercise on Classes and methods in C#.

---
+++ Class_Method_Drill2/Class_Method_Drill2/MathsExercise.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Drill2
+{
+    class MathsExercise
+    {
+        public void Sum(int num1, int num2)
+        {
+            num1 =  num1 + 250;
+            Console.WriteLine(num2);
+            return;
+
+        }
+    }
+}
---
+++ Class_Method_Drill2/Class_Method_Drill2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Drill2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MathsExercise addition = new MathsExercise();
+
+            int x = 200;
+            int y = 800; 
+
+            addition.Sum(200, 800);
+            addition.Sum(num1: x, num2: y);
+
+            Console.ReadLine();
+        }
+    }
+}
