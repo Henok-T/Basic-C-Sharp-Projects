33e6fba45d79be6d540a217308cf9957a73078e0
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-12 14:49:48 -0800

    Class & Methods Drill 3
    An exercise on classes and methods in C#.

---
+++ Class_Method_Drill3/Class_Method_Drill3/MathsExercise.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Drill3
+{
+    class MathsExercise
+    {
+        public int Sum(int num1)
+        {
+            return num1 + 12;
+        }
+
+        public decimal Sum(decimal num1)
+        {
+            return Convert.ToInt32(num1 * 2);
+        }
+
+        public int Sum(string num1)
+        {
+            int num2 = Convert.ToInt32(num1);
+            return num2 / 2;
+        }
+
+    }
+}
---
+++ Class_Method_Drill3/Class_Method_Drill3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Drill3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //takes an int ==> returns an int
+            MathsExercise mathAddition = new MathsExercise();
+            Console.WriteLine("\n\t"+ mathAddition.Sum(12));
+
+            //takes a decimal ==> returns an int
+            MathsExercise mathsMultiply = new MathsExercise();
+            Console.WriteLine("\n\t" + mathsMultiply.Sum(12.5m));
+
+            //takes a string ==> returns an int
+            MathsExercise mathsDivide = new MathsExercise();
+            Console.WriteLine("\n\t" + mathsDivide.Sum("12"));
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
