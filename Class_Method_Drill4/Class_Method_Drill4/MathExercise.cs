d573382b8ee35e6a743bc80a31055611b9ba4d25
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-12 16:09:33 -0800

    Class and methods Drill 4
    An execise on Classes and methods in C#.

---
+++ Class_Method_Drill4/Class_Method_Drill4/MathExercise.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Drill4
+{
+    class MathExercise
+    {
+        public int Multiply(int x, int optionalint = 20)
+        {
+            return Convert.ToInt32(x * 2 * optionalint);
+        }
+    }
+}
---
+++ Class_Method_Drill4/Class_Method_Drill4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Drill4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathExercise mathMultiply = new MathExercise();
+            Console.WriteLine("Please enter two numbers one at a time.");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n\t" + a + " x" + " 2" + " x"+ " the given value of b => " + b + " is equal to " + mathMultiply.Multiply(a, b));
+            Console.WriteLine("\n\t" + a + " x" + " 2" + " x" + " the optional value of b => 20 " + " is equal to " + mathMultiply.Multiply(a));
+            Console.ReadLine();
+
+        }
+
+
+
+        
+    }
+}
