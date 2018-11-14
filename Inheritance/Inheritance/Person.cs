9e589994ec6f74a7cec727f310bd1a2ea5fcc790
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-13 16:00:45 -0800

    Inheritance Drill
    An exercise on Inheritance in C#.

---
+++ Inheritance/Inheritance/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + "[ " + FirstName + "  " +  LastName + " ]");
+        }
+    }
+}
