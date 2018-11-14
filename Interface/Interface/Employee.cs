a449a0ddbab6ae2bb4e8be89d278369b9cbe3922
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-13 21:38:05 -0800

    Interface / Polymorphism
    An exercise on Interface and Polymorphism in C#.

---
+++ Interface/Interface/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            //Console.WriteLine("Name: " + "[ " + FirstName + "  " + LastName + " ]");
+        }
+        
+        public void Quit()
+        {
+            Console.WriteLine("\n\n\n\n\n\n " +
+                "\t\t\t\t  This is a C# \"Interface\" & \"Polymorphism\" in action!!");
+        }
+    }
+}
---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Employee employee = new Employee();
+            employee.Quit();
+
+            IQuittable emIqt = new Employee();
+            emIqt.Quit();
+            Console.ReadLine();
+        }
+    }
+}
