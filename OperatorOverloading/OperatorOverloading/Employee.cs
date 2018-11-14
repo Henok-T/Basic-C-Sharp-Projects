27cc8c6e3f5686b7003e3271b349cc152de08045
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-14 14:29:22 -0800

    OperatorOverloading
    An exercise on operator overloading in C#.

---
+++ OperatorOverloading/OperatorOverloading/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    class Employee
+    {
+        public int Id { get; set; }
+
+        public static bool operator ==(Employee employee_X, Employee employee_Y)
+        {
+            return employee_X.Id == employee_Y.Id;
+        }
+
+        public static bool operator !=(Employee employee_X, Employee employee_Y)
+        {
+            return employee_X.Id != employee_Y.Id;
+        }
+    }
+}
---
+++ OperatorOverloading/OperatorOverloading/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Employee employee_X = new Employee();
+            employee_X.Id = 1000;
+
+            Employee employee_Y = new Employee();
+            employee_Y.Id = 3000;
+
+            Console.WriteLine(employee_X.Id == employee_Y.Id );
+            Console.WriteLine(employee_X.Id != employee_Y.Id);
+
+            Console.ReadLine();
+        }
+    }
+}
