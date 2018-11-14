e81faaf56455f828ca4b42a1bd15f97ac1df3a87
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-13 18:54:42 -0800

    Abstract
    An exercise in Abstract in C#.

---
+++ Polymorph_Abstrct/Polymorph_Abstrct/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorph_Abstrct
+{
+    public class Employee : Person
+
+    { public override void SayName()
+        {
+            Console.WriteLine("Name: " + "[ " + FirstName + "  " + LastName + " ]");
+        }
+    }
+}
---
+++ Polymorph_Abstrct/Polymorph_Abstrct/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorph_Abstrct
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee employee = new Employee();
+
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
