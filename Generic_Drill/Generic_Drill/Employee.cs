f00fecf8c88e4dfb8741470326ad71e8aa3c5102
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-14 16:35:50 -0800

    Generics
    An exercise on Generics in C#.

---
+++ Generic_Drill/Generic_Drill/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Drill
+{
+    public class Employee<T>
+    {
+        public List<T> things { get; set; }
+    }
+}
---
+++ Generic_Drill/Generic_Drill/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Drill
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Employee<string> em1 = new Employee<string>
+            { things = new List<string> { "DOB", "DOE", "Zip-Code", "Salary-Range", "Years Employed", "No.Promotions" }};
+            foreach (string thing in em1.things){Console.WriteLine("\t" + thing +"\n");}
+
+            Employee<int> em2 = new Employee<int>{things = new List<int>
+            { 1960, 2000, 97200, 70000 - 12000, 5 }};
+            foreach (int thing in em2.things){Console.WriteLine("\t" + thing + "\n");}
+
+            Console.ReadLine();
+        }
+    }
+}
