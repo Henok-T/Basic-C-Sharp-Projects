eec8230f69f900fcf98597a181036ce1274b9e3b
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-14 19:54:33 -0800

    Struct Drill
    An exercise on Struct in C#.

---
+++ Struct_Drill/Struct_Drill/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Drill
+{
+    class Program
+    {
+
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Number num = new Number();
+            num.Amount = 10.101m;
+            Console.WriteLine("\n\t The Amount assigned to the \"Struct Number\" is "+ num.Amount);
+            Console.ReadLine();
+        }
+    }
+}
