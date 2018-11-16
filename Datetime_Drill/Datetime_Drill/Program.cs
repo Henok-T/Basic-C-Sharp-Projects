d3cd9485d3cd8b1afd8b5441145f0ed3e3205d0c
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-16 12:51:27 -0800

    Datetime Drill
    An exercise in C#.

---
+++ Datetime_Drill/Datetime_Drill/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime_Drill
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            DateTime d = DateTime.Now;
+            Console.WriteLine( "\n Right now it is  "+ d + ".");
+
+            Console.WriteLine("\n Please enter a whole number.\n");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n In " + x + "hrs from now it will be " + d.AddHours(x) + ".");
+
+
+            Console.ReadLine();
+        }
+    }
+}
