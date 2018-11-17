92491b1c07e2468d282b4c450d2816868524b688
Henok <31227367+Henok-Tsegai@users.noreply.github.com>
2018-11-17 00:21:01 -0800

    Constructor Chaining... + additional features
    an exercise in C#.

---
+++ AdditionalFeatures/AdditionalFeatures/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeatures
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int remainingDays = 44;
+            Console.WriteLine("\n I'm left with {0} remaining days of access to the LMS.", remainingDays );
+
+            var comingHoliday = new DateTime(2018,11,22);
+            Console.WriteLine("\n Thanksgiving is next week on {0}", comingHoliday + "\n\n\t\t HAPPY" + "\n\t THANKSGIVING");
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AdditionalFeatures/AdditionalFeatures/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeatures
+{
+    class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public Student (int id, string name)
+        { this.id = id;
+          this.name = name;
+        }
+
+        public Student (int id) : this ( id, "")
+        {
+           
+        }
+        public Student(string name) : this(0, name)
+        {
+
+        }
+
+    }
+}
